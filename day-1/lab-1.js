5f08e52c468357eb917a70a61348c3d2e3f8a31a
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-03-12 15:07:53 +0200

    lab reviewed

--- day-1/lab-1.js
+++ day-1/lab-1.js
@@ -1,71 +1,47 @@
 // 1
-/*
 var y;
 console.log(y); // undefined
-*/
 
 // 2
-/*
 console.log(y); // error: y is not defined
-*/
 
 // 3
-/*
 var x = 10;
-var y=20;
-console.log(y-x*2); // 0
-*/
+var y = 20;
+console.log(y - x * 2); // 0
 
 // 4
-/*
 var y;
 console.log(typeof y); // undefined
-*/
 
 // 5
-/*
 var x = "1";
 var y = 2;
-console.log(x+y); // NOT 3 BUT "12"
-*/
+console.log(x + y); // NOT 3 BUT "12"
 
 // 6
-/*
 var x = 1;
 var y = true;
-console.log(x+y); // 2
-*/
+console.log(x + y); // 2
 
 // 7
-/*
-var x =12;
-var y ="6";
-console.log(x/y); //2
-*/
+var x = 12;
+var y = "6";
+console.log(x / y); //2
 
 // 8
-/*
 console.log(15 + 3 + "number"); // NOT "153number" BUT "18number"
 console.log("number" + 15 + 3); // NOT "number18" BUT "number153"
-*/
 
 // 9
-/*
 var x = false;
 var y = !!x; // => !(!x) => !(!false) => !(true) => false
 console.log(y); // false
-*/
 
 // 10
-/*
 var x = "menna";
 console.log(typeof x == "string"); // true
-*/
 
 // 11
-/*
 var x = false;
 console.log(typeof x == 0); // flase
-*/
-
-
--- day-1/search.js
+++ day-1/search.js
@@ -1,20 +1,21 @@
 // CONVERTING
-/*
 // Convert from string to number
 var quantity = "12";
 console.log(typeof quantity);
 
 var quatityToNum = Number(quantity); // using Number() function
+console.log(Number("eslam")); // error: unexpected string
+console.log(Number(true)); // 1
 var quatityToNum2 = parseInt(quantity); // using parseInt() function
+console.log(parseInt("eslam")); // NaN
 console.log(typeof quatityToNum);
 console.log(typeof quatityToNum2);
 
 // Convert from number to string
 var num = 77;
-var numToString = num.toString()
+var numToString = num.toString();
 console.log(typeof num);
 console.log(typeof numToString);
-*/
 
 // CHECKING
 // Checking if a variable is a string!?
@@ -22,7 +23,7 @@ var z = "Hakuna Matata";
 console.log("10" instanceof String);
 
 // Checking if a variable is a number!?
-// var x = 10;
-// var y = "Eslam";
-// console.log(isNaN(x)); // false
-// console.log(isNaN(y)); // true
+var x = 10;
+var y = "Eslam";
+console.log(isNaN(x)); // false
+console.log(isNaN(y)); // true
