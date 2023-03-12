b81f0d11a10be409fb4cb4b3827ced991507be0c
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-03-12 14:10:00 +0200

    finished lab-1

---
+++ day-1/search.js
@@ -0,0 +1,28 @@
+// CONVERTING
+/*
+// Convert from string to number
+var quantity = "12";
+console.log(typeof quantity);
+
+var quatityToNum = Number(quantity); // using Number() function
+var quatityToNum2 = parseInt(quantity); // using parseInt() function
+console.log(typeof quatityToNum);
+console.log(typeof quatityToNum2);
+
+// Convert from number to string
+var num = 77;
+var numToString = num.toString()
+console.log(typeof num);
+console.log(typeof numToString);
+*/
+
+// CHECKING
+// Checking if a variable is a string!?
+var z = "Hakuna Matata";
+console.log("10" instanceof String);
+
+// Checking if a variable is a number!?
+// var x = 10;
+// var y = "Eslam";
+// console.log(isNaN(x)); // false
+// console.log(isNaN(y)); // true
